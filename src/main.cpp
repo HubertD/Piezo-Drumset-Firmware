136696cce592fd555b28d3873c92370e13da66f6
Hubert Denkmair <Hubert Denkmair>
2019-10-17 20:43:34 +0200

    cleanup main module

--- src/main.cpp
+++ src/main.cpp
@@ -12,72 +12,20 @@
 
 using namespace LibreUCpp::HAL;
 
-volatile uint32_t TIME = 0;
+static Clock clk;
+static UART uart;
+static ADC adc;
+static volatile uint32_t TIME = 0;
 
-extern "C" {
-    void SysTick_Handler()
-    {
-        TIME++;
-    }
-}
-
-Clock clk;
-UART uart;
-ADC adc;
-
-void SetupAdc()
-{
-    Pin gnd { Port::A[3] };
-    gnd.SetDirection(Pin::Direction::OUTPUT);
-    gnd.Write(false);
-
-    Pin ain0 { Port::A[2] };
-    ain0.ConfigureMultiplex(Pin::Mux::B);
-
-    Pin ain2 { Port::B[8] };
-    ain2.ConfigureMultiplex(Pin::Mux::B);
-
-    Pin ain3 { Port::B[9] };
-    ain3.ConfigureMultiplex(Pin::Mux::B);
-
-    adc.Setup(
-        ADC::INSTANCE::ADC0,
-        GCLK::GENERATOR::Generator_0,
-        ADC::PRESCALER::DIV_BY_8,
-        ADC::REFSEL::INTVCC2,
-        ADC::RESOLUTION::RESOLUTION_10_BIT,
-        ADC::DIFFMODE::SINGLE_ENDED,
-        ADC::ACCUMULATION::ACCUMULATE_1_SAMPLE
-    );
-}
-
-void SetupUart()
-{
-    Pin txd { Port::B[2] };
-    txd.ConfigureMultiplex(Pin::Mux::D);
-
-    Pin rxd { Port::B[3] };
-    rxd.ConfigureMultiplex(Pin::Mux::D);
-
-
-    uart.Setup(
-        UART::INSTANCE::SERCOM5, 
-        GCLK::GENERATOR::Generator_0,
-        UART::RXPAD::RXPO1, UART::TXPAD::TXPO0,
-        GCLK::CalcFrequency(GCLK::GENERATOR::Generator_0, clk.GetXosc32kFrequency(), clk.GetXoscFrequency())
-    );
-    uart.Init(115200, UART::BITS::EIGHT, UART::PARITY::NONE, UART::STOP_BITS::ONE);
-}
-
+int main();
+extern "C" void SysTick_Handler();
+static void SetupClock();
+static void SetupAdc();
+static void SetupUart();
 
 int main()
 {
-    clk.Configure(ClockConfig::Get48MHzFrom16MHzCrystal());
-
-    Systick::SetFrequency(clk, 1000);
-    Systick::EnableInterrupt();
-    Systick::Enable();
-
+    SetupClock();
     SetupAdc();
     SetupUart();
 
@@ -107,4 +55,60 @@ int main()
             }
         }
     }
+}
+
+extern "C" void SysTick_Handler()
+{
+    TIME++;
+}
+
+static void SetupClock()
+{
+    clk.Configure(ClockConfig::Get48MHzFrom16MHzCrystal());
+    Systick::SetFrequency(clk, 1000);
+    Systick::EnableInterrupt();
+    Systick::Enable();
+}
+
+static void SetupAdc()
+{
+    Pin gnd { Port::A[3] };
+    gnd.SetDirection(Pin::Direction::OUTPUT);
+    gnd.Write(false);
+
+    Pin ain0 { Port::A[2] };
+    ain0.ConfigureMultiplex(Pin::Mux::B);
+
+    Pin ain2 { Port::B[8] };
+    ain2.ConfigureMultiplex(Pin::Mux::B);
+
+    Pin ain3 { Port::B[9] };
+    ain3.ConfigureMultiplex(Pin::Mux::B);
+
+    adc.Setup(
+        ADC::INSTANCE::ADC0,
+        GCLK::GENERATOR::Generator_0,
+        ADC::PRESCALER::DIV_BY_8,
+        ADC::REFSEL::INTVCC2,
+        ADC::RESOLUTION::RESOLUTION_10_BIT,
+        ADC::DIFFMODE::SINGLE_ENDED,
+        ADC::ACCUMULATION::ACCUMULATE_1_SAMPLE
+    );
+}
+
+static void SetupUart()
+{
+    Pin txd { Port::B[2] };
+    txd.ConfigureMultiplex(Pin::Mux::D);
+
+    Pin rxd { Port::B[3] };
+    rxd.ConfigureMultiplex(Pin::Mux::D);
+
+    uart.Setup(
+        UART::INSTANCE::SERCOM5, 
+        GCLK::GENERATOR::Generator_0,
+        UART::RXPAD::RXPO1, UART::TXPAD::TXPO0,
+        GCLK::CalcFrequency(GCLK::GENERATOR::Generator_0, clk.GetXosc32kFrequency(), clk.GetXoscFrequency())
+    );
+    uart.Init(115200, UART::BITS::EIGHT, UART::PARITY::NONE, UART::STOP_BITS::ONE);
 }
